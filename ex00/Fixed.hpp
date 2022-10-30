8862bed2ea97fb149711adbf3574957e4f631bd2
Alejandro Aguilera garcia <aaguiler@c3r5s2.42malaga.com>
2022-10-30 20:11:52 +0100

    Add ex00

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaguiler <aaguiler@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 19:56:54 by aaguiler          #+#    #+#             */
+/*   Updated: 2022/10/30 20:06:06 by aaguiler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+class Fixed
+{
+private:
+	int					_value;
+	static const int	_fractionalBits = 8;
+public:
+	Fixed(void);
+	Fixed(const Fixed &src);
+	~Fixed(void);
+
+	Fixed	&operator=(const Fixed &rhs);
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aaguiler <aaguiler@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 19:56:52 by aaguiler          #+#    #+#             */
+/*   Updated: 2022/10/30 20:08:02 by aaguiler         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main( void ) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+	c = b;
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+	return 0;
+}
